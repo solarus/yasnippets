555a7a0f1b1b9ddc53964546870c6e8b132e8dbb
solarus <solarus@tunna.org>
2011-03-16 17:58:06 +0100

    initial commit

---
+++ yas-haskell-funs.el
@@ -0,0 +1,15 @@
+(defun my-get-haskell-modules ()
+  (with-temp-buffer
+    (insert (shell-command-to-string "ghc-pkg dump"))
+    (goto-char (point-min))
+    (let ((last-point (point-min)))
+      (while (search-forward "exposed-modules: " nil t)
+        (goto-char last-point)
+        (delete-region last-point (search-forward "exposed-modules: "))
+        (setq last-point (- (search-forward "hidden-modules:") 16)))
+      (delete-region last-point (point-max)))
+    (goto-char (point-min))
+    (replace-regexp "[ \n\t]+" " ")
+    (split-string (buffer-string))))
+
+(provide 'yas-haskell-funs)
